b49dba9bdd5e0da8630411578bf034b4bd196be9
LiuLang <gsushzhsosgsu@gmail.com>
2013-09-13 12:28:32 +0800

    Use TreeViewSongs

--- kuwo/TopList.py
+++ kuwo/TopList.py
@@ -3,6 +3,7 @@ from gi.repository import GdkPixbuf
 from gi.repository import Gtk
 
 from kuwo import Net
+from kuwo import Widgets
 
 NID = 2
 
@@ -11,6 +12,7 @@ class TopList(Gtk.Box):
         super().__init__()
         self.set_orientation(Gtk.Orientation.VERTICAL)
         self.app = app
+        self.first_show = False
 
         self.buttonbox = Gtk.Box(spacing=8)
         self.pack_start(self.buttonbox, False, False, 0)
@@ -43,10 +45,10 @@ class TopList(Gtk.Box):
         self.scrolled_nodes = Gtk.ScrolledWindow()
         self.pack_start(self.scrolled_nodes, True, True, 0)
 
-        iconview_nodes = Gtk.IconView()
         # logo, name, nid
+        # TODO: use Widgets.IconView
         self.liststore_nodes = Gtk.ListStore(GdkPixbuf.Pixbuf, str, int)
-        iconview_nodes.set_model(self.liststore_nodes)
+        iconview_nodes = Gtk.IconView(model=self.liststore_nodes)
         iconview_nodes.set_pixbuf_column(0)
         iconview_nodes.set_text_column(1)
         iconview_nodes.set_item_width(95)
@@ -57,60 +59,10 @@ class TopList(Gtk.Box):
         self.scrolled_songs = Gtk.ScrolledWindow()
         self.pack_start(self.scrolled_songs, True, True, 0)
 
-        treeview_songs = Gtk.TreeView()
         # checked, name, artist, album, rid, artistid, albumid
         self.liststore_songs = Gtk.ListStore(bool, str, str, str, int, int,
                 int, GdkPixbuf.Pixbuf, GdkPixbuf.Pixbuf, GdkPixbuf.Pixbuf)
-        treeview_songs.set_model(self.liststore_songs)
-        treeview_songs.set_headers_visible(False)
-        treeview_songs.connect('row_activated', 
-                self.on_treeview_songs_row_activated)
-        self.scrolled_songs.add(treeview_songs)
-
-        checked = Gtk.CellRendererToggle()
-        checked.connect('toggled', self.on_song_checked)
-        column_check = Gtk.TreeViewColumn('Checked', checked, active=0)
-        treeview_songs.append_column(column_check)
-
-        name = Gtk.CellRendererText()
-        col_name = Gtk.TreeViewColumn('Name', name, text=1)
-        col_name.props.sizing = Gtk.TreeViewColumnSizing.AUTOSIZE
-        #col_name.props.min_width = 100
-        #col_name.props.max_width = 520
-        col_name.props.expand = True
-        treeview_songs.append_column(col_name)
-
-        artist = Gtk.CellRendererText()
-        col_artist = Gtk.TreeViewColumn('Artist', artist, text=2)
-        col_artist.props.sizing = Gtk.TreeViewColumnSizing.AUTOSIZE
-        col_artist.props.expand = True
-        treeview_songs.append_column(col_artist)
-
-        album = Gtk.CellRendererText()
-        col_album = Gtk.TreeViewColumn('Album', album, text=3)
-        col_album.props.sizing = Gtk.TreeViewColumnSizing.AUTOSIZE
-        col_album.props.expand = True
-        treeview_songs.append_column(col_album)
-
-        play = Gtk.CellRendererPixbuf()
-        col_play = Gtk.TreeViewColumn('Play', play, pixbuf=7)
-        col_play.props.sizing = Gtk.TreeViewColumnSizing.FIXED
-        col_play.props.fixed_width = 20
-        treeview_songs.append_column(col_play)
-
-        add = Gtk.CellRendererPixbuf()
-        col_add = Gtk.TreeViewColumn('Add', add, pixbuf=8)
-        col_add.props.sizing = Gtk.TreeViewColumnSizing.FIXED
-        col_add.props.fixed_width = 20
-        treeview_songs.append_column(col_add)
-
-        cache = Gtk.CellRendererPixbuf()
-        col_cache = Gtk.TreeViewColumn('Cache', cache, pixbuf=9)
-        col_cache.props.sizing = Gtk.TreeViewColumnSizing.FIXED
-        col_cache.props.fixed_width = 20
-        treeview_songs.append_column(col_cache)
-
-        self.first_show = False
+        treeview_songs = Widgets.TreeViewSongs(self.liststore_songs, app)
 
     def after_init(self):
         self.buttonbox.hide()
